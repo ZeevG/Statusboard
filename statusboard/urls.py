77787bd099e4bd2fcda903208ad66d75a2e7962e
Ze'ev Gilovitz <zeev.gil@gmail.com>
2014-05-08 22:25:55 +0800

    initial backend commit

---
+++ statusboard/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from forecast.views import ProxyDetailView
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+
+    # Examples:
+    # url(r'^$', 'statusboard.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^forecast/(?P<pk>[0-9]+)$', ProxyDetailView.as_view(), name='item-detail')
+)
